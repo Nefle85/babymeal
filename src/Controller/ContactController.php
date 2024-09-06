33fcef351aa75a56efa4de3afaac467363d70642
cindy Guignard <cindyg0382@gmail.com>
2024-09-06 14:07:15 +0200

    contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,40 @@
+<?php
+
+// src/Controller/ContactController.php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Save the contact in the database
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            // Add a flash message or redirect the user after successful form submission
+            $this->addFlash('success', 'Your message has been sent successfully!');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
